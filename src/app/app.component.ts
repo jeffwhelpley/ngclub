720b73f4ce667c077ae1082a4fba6b4cc5af9001
Jeff Whelpley <jeff@gethuman.com>
2019-01-05 22:53:02 -0500

    Initial version out there

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { toolbox } from './toolbox';
+
+declare var Blockly: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
-export class AppComponent {
-  title = 'ngclub';
+export class AppComponent implements OnInit {
+  workspace: any;
+  code: string;
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.workspace = Blockly.inject('blocklyDiv', { toolbox: toolbox });
+      this.workspace.addChangeListener(() => {
+        Blockly.JavaScript.INFINITE_LOOP_TRAP = null;
+        this.code = Blockly.JavaScript.workspaceToCode(this.workspace);
+      });
+    }, 2000);
+  }
+
+  runProgram() {
+    /* tslint:disable */
+    eval(this.code);
+  }
 }
