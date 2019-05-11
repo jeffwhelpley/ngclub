aac9f6fff7fcd7b87ccf0d9228a192c61c4afab3
Jeff Whelpley <jeff@gethuman.com>
2019-05-11 07:14:48 -0400

    Fix after removing root component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RootComponent } from './root.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    RootComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent],
+    imports: [BrowserModule, AppRoutingModule],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
